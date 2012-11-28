a8a3e50d2acabb07efdf9c6bc5fb57dd6fb9a789
congchenutd <congchenutd@09d2a836-7f58-ebf1-d5f4-391bf92412f9>
2012-11-28 04:19:40 +0000

    = rectangular boundary

--- BoundaryGuard.cpp
+++ BoundaryGuard.cpp
@@ -1,114 +1,21 @@
 #include "BoundaryGuard.h"
-#include "Edge.h"
 #include "Node.h"
-#define _USE_MATH_DEFINES
-#include <cmath>
 #include <QGraphicsItem>
-#include <QDebug>
 
 namespace ForceDirectedLayout
 {
 
 bool Boundary::isEscaping(const QLineF& path) const {
-    return isInside(path.p1()) && !isInside(path.p2());
+    return isInside(path.p1()) && !isInside(path.p2());  // one in, one out
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////
-void PolygonalBoundary::adjustEscapingPath(QLineF& escapingPath) const
-{
-    foreach(const QLineF& edge, getEdges())
-    {
-        if(isEscaping(edge, escapingPath))
-        {
-            QPointF target = escapingPath.p2();
-            adjustEscapingPath(edge, escapingPath);
-            QPointF result = escapingPath.p2();
-            qDebug() << "target = " << target << "stop at = " << result;
-            Q_ASSERT(result.x() >= 0.0);
-        }
-    }
-}
-
-void PolygonalBoundary::adjustEscapingPath(const QLineF& edge, QLineF& escapingPath) const
-{
-    qreal alpha = escapingPath.angleTo(edge);
-    qreal beta  = mod2PI(alpha + 270);
-    qreal gamma = mod2PI(beta + 270);
-
-    // normal component
-    QLineF normalComponent = escapingPath;
-    qreal normalAngle = mod2PI(escapingPath.angle() + beta);
-    normalComponent.setAngle(normalAngle);
-
-    QPointF normalCrossing;
-    if(edge.intersect(normalComponent, &normalCrossing) == QLineF::NoIntersection)
-        return;
-
-    QLineF normalComponentCut(escapingPath.p1(), normalCrossing);
-
-    // parallel component
-    QLineF parallelComponent = escapingPath;
-    qreal parallelAngle = mod2PI(escapingPath.angle() + gamma);
-    parallelComponent.setAngle(parallelAngle);
-    parallelComponent.setLength(escapingPath.length() * sin(beta/180*M_PI));
-
-    // combine two components
-    qreal x = escapingPath.p1().x();
-    x += normalComponentCut.p2().x() - normalComponentCut.p1().x();
-    x += parallelComponent.p2().x() - parallelComponent.p1().x();
-
-    qreal y = escapingPath.p1().y();
-    y += normalComponentCut.p2().y() - normalComponentCut.p1().y();
-    y += parallelComponent.p2().y() - parallelComponent.p1().y();
-
-    escapingPath.setP2(QPointF(x, y));
-}
-
-// return angle % 360
-qreal PolygonalBoundary::mod2PI(qreal angle) const
-{
-    qreal result = angle;
-    while(result >= 360)
-        result -= 360;
-    return result;
-}
-
-bool PolygonalBoundary::isEscaping(const QLineF& edge, const QLineF& path) const {
-    return isInside(edge, path.p1()) && !isInside(edge, path.p2());
-}
-
-bool PolygonalBoundary::isInside(const QLineF& edge, const QPointF& point) const
-{
-    QLineF edgeStart2Point(edge.p1(), point);
-    return edge.angleTo(edgeStart2Point) <= 180;
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////
-bool TriangularBoundary::isInside(const QPointF& point) const {
+bool Boundary::isInside(const QPointF& point) const {
     return contains(point);
 }
 
-QList<QLineF> TriangularBoundary::getEdges() const
+QList<QLineF> Boundary::getEdges() const
 {
-    QList<QLineF> result;
-    QPolygonF triangle = polygon();
-    if(triangle.size() != 3)
-        return result;
-
-    result << QLineF(triangle[0], triangle[1])
-           << QLineF(triangle[1], triangle[2])
-           << QLineF(triangle[2], triangle[0]);
-    return result;
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////
-bool RectangularBoundary::isInside(const QPointF& point) const {
-    return contains(point);
-}
-
-QList<QLineF> RectangularBoundary::getEdges() const
-{
-    QList<QLineF> edges;
+    QList<QLineF> edges;    // edges (vertices) are added counter-clockwise
     edges << QLineF(rect().topRight(),    rect().topLeft())
           << QLineF(rect().topLeft(),     rect().bottomLeft())
           << QLineF(rect().bottomLeft(),  rect().bottomRight())
@@ -116,28 +23,51 @@ QList<QLineF> RectangularBoundary::getEdges() const
     return edges;
 }
 
+qreal Boundary::getAspectRatio() const {
+    return rect().height() == 0 ? 1.0
+                                : rect().width() / rect().height();
+}
+
+bool Boundary::isInside(const QLineF& edge, const QPointF& point)
+{
+    QLineF edgeStart2Point(edge.p1(), point);   // a line from start of the edge to point
+    return edge.angleTo(edgeStart2Point) <= 180;
+}
 
 /////////////////////////////////////////////////////////////////////////////
 BoundaryGuard::BoundaryGuard(Boundary* boundary)
     : _boundary(boundary)
 {}
 
+void BoundaryGuard::guard(Node* node, qreal& xMove, qreal& yMove)
+{
+    if(node == 0 || _boundary == 0)
+        return;
+
+    QLineF escapingPath(node->pos(), node->pos() + QPointF(xMove, yMove));
+    if(!_boundary->isEscaping(escapingPath))
+        return;
+
+    foreach(const QLineF& edge, _boundary->getEdges())     // for each edge
+        if(!_boundary->isInside(edge, escapingPath.p2()))  // if escape from this edge
+            adjustEscapingPath(edge, escapingPath);        // block the path
+
+    xMove = escapingPath.x2() - escapingPath.x1();
+    yMove = escapingPath.y2() - escapingPath.y1();
+}
+
+/////////////////////////////////////////////////////////////////////////////
 AdhesiveBoundaryGuard::AdhesiveBoundaryGuard(Boundary* boundary)
     : BoundaryGuard(boundary)
 {}
 
-void AdhesiveBoundaryGuard::guard(Node* node, qreal& xMove, qreal& yMove)
+void AdhesiveBoundaryGuard::adjustEscapingPath(const QLineF& edge, QLineF& escapingPath)
 {
-    if(_view == 0 || node == 0)
-        return;
-
-    QLineF path(node->pos(), node->pos() + QPointF(xMove, yMove));
-    if(_boundary->isEscaping(path))
-    {
-        _boundary->adjustEscapingPath(path);
-        xMove = path.x2() - path.x1();
-        yMove = path.y2() - path.y1();
-    }
+    if(edge.x1() == edge.x2())   // left or right edge
+        escapingPath.setP2(QPointF(edge.x1(), escapingPath.y2()));  // use edge's x, path's y
+    else                         // bottom or top
+        escapingPath.setP2(QPointF(escapingPath.x2(), edge.y1()));  // path's x, edge's y
 }
+
 
 } // namespace
